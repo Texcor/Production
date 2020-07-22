be4e05e1e3f6df44fc984b4e6ad9ce9631159be9
Ana-Gabriel <agp@odoo.com>
2020-07-21 17:20:27 -0700

    [ADD] texcor_network_mgmt: ip management model + view

--- texcor_network_mgmt/models/ip_management.py
+++ texcor_network_mgmt/models/ip_management.py
@@ -21,14 +21,40 @@ class Circuits(models.Model):
         ('/30', '/30'),
     ]
 
-    name = fields.Char(string='Network')
-    network_ip = fields.Char(string='Network')
-    gateway_ip = fields.Char(string='Gateway IP')
-    status = fields.Selection(CIDR, 'CIDR')
+    AVAILABILITY = [
+        ('assigned', 'Assigned'),
+        ('reserved', 'Reserved'),
+        ('available', 'Available'),
+    ]
 
+    name = fields.Char(string='Network')
+    
+    net_mask = fields.Char(string='Net Mask')
+    network_ip = fields.Char(string='Network')
+    provider_ip = fields.Char(string='Gateway IP')
+    network_address = fields.Char(string='Network Address')
+
+    cidr = fields.Selection(CIDR, 'CIDR')
+    availability = fields.Selection(AVAILABILITY, 'Availability')
+
+    management_ip = fields.Boolean(string='Management IP')
     parent_network = fields.Boolean(string='Parent Network')
     internal_use = fields.Boolean(string='Internal Use')
 
+    equipment_id = fields.Many2one(string='Equipment', comodel_name='equipment', ondelete='cascade')
     circuit_id = fields.Many2one(string='Circuit IDs', comodel_name='circuit', ondelete='cascade')
     location_id = fields.Many2one(string='Service Location', comodel_name='res.partner', ondelete='cascade')
     
+    description = fields.Text(string='Description')
+    network_cidr = fields.Char(string='Network Address/CIDR')
+
+    @api.depends('cidr', 'network_address')
+    def _compute_network_cidr(self):
+        if self.cidr == False:
+            cidr = str("/??")
+        for record in self:
+            if self.cidr == False:
+                cidr = str("/??")
+            else:
+                cidr = str(record.cidr)
+                record['network_cidr'] = str(record.network_address + cidr)
